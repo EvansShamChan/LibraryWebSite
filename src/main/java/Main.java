f1ddd794de8c19fa426c2396327fd7417df35f58
KKhrystyna <khrystynavolosatska@gmail.com>
2018-06-28 23:49:08 +0300

    Fixed structure (webapp). Created one entity, one dao, connection manager. Configured web.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import com.softserve.edu.library.dao.BookDao;
+import com.softserve.edu.library.entity.Book;
+
 public class Main {
+    public static void main(String[] args) {
+        BookDao bookDao = new BookDao();
+        Book book = bookDao.getById("4");
+        System.out.println(book);
+
+    }
 }
